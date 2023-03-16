afffa4e794d0df51ef5cea3284cf1c9196aec8bb
olegblyzniuk <mettal062@gmail.com>
2023-03-16 14:35:38 +0200

    homework 2

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log('Hello')
-console.log('Hello World!')
+//Task №1
+
+let firstStr = 'I';
+let secondStr = 'learn';
+let thirdStr = 'JS';
+console.log(firstStr+' '+secondStr+' '+thirdStr)
+
+//Task №2
+
+console.log('5 '+'7 '+'7 '+'4 '+'9')
