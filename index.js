12fc2d6fbc6a73e0e41a732cc9b8bb85029ffdf9
olegblyzniuk <mettal062@gmail.com>
2023-03-26 16:14:31 +0300

    homework 6,7,8

--- index.js
+++ index.js
@@ -1,38 +1,93 @@
-//Homework #3
+//Homework #6
 
-let firstRequest = prompt('Первое число');
-let secondRequest = prompt('Второе число');
-let firstNum = Number(firstRequest);
-let secondNum = Number(secondRequest);
+let requestAge = prompt('Ваш год рождения?');
+let userTown = prompt('В каком городе живете?');
+let likeSport = prompt('Ваш любимый вид спорта?');
 
-let resSum = firstNum + secondNum;
-let resSubstr = firstNum - secondNum;
-let resMult = firstNum * secondNum;
-let resDivision = firstNum / secondNum;
+let championFootball = 'Lionel Messi';
+let championSwimming = 'Michael Phelps';
+let championFormula = 'Michael Schumacher';
 
-alert(`${firstNum} + ${secondNum} = ${resSum}
-${firstNum} + ${secondNum} = ${resSubstr}
-${firstNum} * ${secondNum} = ${resMult}
-${firstNum} / ${secondNum} = ${resDivision}`)
+let ifCapital
+if(userTown === "Kyiv"){
+    ifCapital = "Вы проживаете в столице Украины"
+} else if(userTown === "London"){
+    ifCapital = "Вы проживаете в столице Великобритании"
+} else if(userTown === "Washigton"){
+    ifCapital = "Вы проживаете в столице США"
+} else if(userTown === null){
+    ifCapital= "Жаль, Вы не захотели указывать свой город"
+}else{ifCapital = `Вы проживаете в городе ${userTown}`}
 
-//Homework #4
+let year = Number(requestAge);
+let calcAge = 2023-year;
+let userAge
+if(year===0){
+    userAge="Жаль, Вы не захотели указывать свой год рождения"
+}else{userAge=`Ваш  возраст: ${calcAge}`}
 
-let requestHours = prompt('Сколько часов');
-let hours = Number(requestHours);
+let yourSport
+if(likeSport === "football"){
+    yourSport = `Круто! Хочешь стать как ${championFootball}?`
+}else if(likeSport === "swimming"){
+    yourSport = `Круто! Хочешь стать как ${championSwimming}?`
+}else if(likeSport === "formula1"){
+    yourSport = `Круто! Хочешь стать как ${championFormula}?`
+}else if(likeSport === null){
+    yourSport = "Жаль, Вы не захотели указывать свой любимый вид спорта"
+}else{yourSport = `Ваш любимый вид спорта ${likeSport}`}
 
-let secondsInHours = hours * 360;
-alert(secondsInHours);
+alert(`${userAge}
+${ifCapital}
+${yourSport}`)
 
-//Homework #5
+//Homework #7
 
-let firstNumRequest = prompt('Первое число');
-let secondNumRequest = prompt('Второе число');
-let thirdNumRequest = prompt('Третье число');
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
-let oneNum = Number(firstNumRequest);
-let twoNum = Number(secondNumRequest);
-let threeNum = Number(thirdNumRequest);
+switch(numOrStr){
+    case null:
+        console.log('ви скасували');
+        break;
+    case  '':
+        console.log('Empty String');
+        break;
+    case isNaN( +numOrStr ):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!')
+        break;
+}
 
-let arithmeticalMean = (oneNum+twoNum+threeNum)/3;
+//Homework #8
 
-alert(arithmeticalMean);
+let toDo = prompt('Что вы хотите сделать: add, sub, mult, div?');
+let firstNumReq = prompt ('Первое число');
+let secondNumReq = prompt('Второе число');
+
+let firstNum = Number(firstNumReq);
+let secondNum = Number(secondNumReq);
+
+let resultToDo
+
+switch(toDo){
+    case 'add':
+        resultToDo = firstNum + secondNum;
+        alert(`${firstNum} + ${secondNum} = ${resultToDo}`);
+        break;
+    case 'sub':
+        resultToDo = firstNum - secondNum;
+        alert(`${firstNum} - ${secondNum} = ${resultToDo}`);
+        break;
+    case 'mult':
+        resultToDo = firstNum * secondNum;
+        alert(`${firstNum} * ${secondNum} = ${resultToDo}`);
+        break;
+    case 'div':
+        resultToDo = firstNum / secondNum;
+        alert(`${firstNum} / ${secondNum} = ${resultToDo}`);
+        break;
+}
+
