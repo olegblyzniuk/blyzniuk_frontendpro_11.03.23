c14d88813ae0271dbc1e9c64a6e1555dc7a6f40d
olegblyzniuk <mettal062@gmail.com>
2023-04-10 20:14:20 +0300

    homework 10,12,13,14

--- index.js
+++ index.js
@@ -1,83 +1,98 @@
-//Homework #9
+//Homework #10
 
-let i=10;
-while(i<20){
-    console.log(i);
-    i++;
+//Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).
+let str = '';
+for(c=20; c<=30; c+=0.5){
+    str+= c+' ';
+}
+console.log(str)
+
+//Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
+let priceDollar = 27;
+for(q=0; q<=100; q+=10){
+    let mustPay = q *priceDollar;
+    console.log(mustPya)
 }
 
-let p=10;
-while(p<20){
-    console.log(p**2);
-    p++;
-}
-
-let mainCounter = 7;
-let counter = 1;
-while(counter<=10){
-    resMult = mainCounter*counter;
-    console.log(`${mainCounter}*${counter}=${resMult}`)
-    counter++
-}
-
-let sumOfNum = 0;
-for(k=0; k<=15; k++){
-    sumOfNum+=k;
-}
-console.log(sumOfNum)
-
-let multOfNum = 1;
-for(j=15; j<=35; j++){
-    multOfNum*=j;
-}
-console.log(multOfNum)
-
-let sumOfNum2 = 0;
-for(l=0; l<=500; l++){
-    sumOfNum2+=l;
-}
-console.log(sumOfNum2)
-console.log(sumOfNum2/500)
-
-let sumOfNum3 = 0;
-for  (n=30; n<=80; n++){
-    if(n%2 === 0){
-        sumOfNum3 += n;
+//Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
+let someNumber = 10;
+let isSimple;
+let canGet;
+for(y=1;y<=100;y++){
+    if(y**2 < someNumber){
+        console.log(y)
     }
 }
-console.log(sumOfNum3)
-
-for( let m=100; m<=200; m++){
-    if(m%3 === 0){
-        console.log(m)
-    }
+//Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
+if(someNumber <= 1){
+    console.log('Число должно быть больше 1');
 }
+for(e=2; e<someNumber; e++){
+    if(someNumber%e === 0){
+        isSimple = 'Число не простое';
+        break;
+     }else{isSimple = 'Число простое';}
+}
+console.log(isSimple);
 
-let someNum = prompt('Натуральное число?');
-let divider;
-let naturalNum = Number(someNum);
-let sumOfDividers=0;
-console.log(`Натуральное число ${naturalNum}`)
-for(b=1; b<=naturalNum; b++){
-        if(naturalNum%b === 0){
-        	divider = b;
-            console.log(`делится на ${divider}`)
+//Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
+for(g=1; g<someNumber; g++){
+    if(3**g === someNumber){
+        canGet = `Можем получить число ${someNumber}`;
+        break;
+     }else{
+     canGet = `Не можем получить число ${someNumber}`;
+     }
+}
+console.log(canGet);
+
+
+//Homework #12
+//Реалізуйте функцію removeElement
+
+const array = [14, 24, 3, 7, 20, 4, 2];
+
+function removeElement (arr, n){
+	let someArr =[];
+    for( i = 0; i < arr.length; i++){
+        if( filter(arr[i], n)){
+        someArr.push(arr[i])
         }
     }
- for(divider=1; divider<=naturalNum; divider++){
-    if(naturalNum%divider === 0 && divider%2 === 0){
-        console.log(`делится на парный делитель ${divider}`)
-        sumOfDividers+=divider
-    }
+    console.log(someArr)
 }
-console.log(sumOfDividers)
+function filter(el, n){
+    if(el !== n) {
+    return el
+    }
+  } 
+removeElement(array, 24);
 
-let firstCounter;
-let secondCounter;
-let result;
-for(firstCounter=1;firstCounter<=10;firstCounter++){
-    for(secondCounter=1; secondCounter<=10;secondCounter++){
-        result= firstCounter * secondCounter;
-        console.log(`${firstCounter} * ${secondCounter} = ${result}`)
+//Homework #13
+//Реалізуйте функцію generateKey
+
+const length = 6;
+const characters ='abcdefghijklmnopqrstuvwxyz0123456789';
+function generateKey(length, characters) {
+    let password = '';
+    for(let i = 0; i < length; ++i){
+       password += characters[
+           Math.floor(
+               Math.random() * characters.length
+          )
+      ];
     }
-}
+    console.log(password);
+ }
+ generateKey(length, characters)
+
+ //Homework #14
+ //Рекурсивне зведення в ступінь
+
+ function exponent(n, deg){
+    let number;
+    if (deg<=0){
+        return 1;
+    } else {
+        return n*exponent(n, deg-1)}
+ }
