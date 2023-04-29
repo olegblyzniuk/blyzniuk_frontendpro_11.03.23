9fdf11068f9c8ba0cab59db685e80fcbcf5e2f53
olegblyzniuk <mettal062@gmail.com>
2023-04-29 18:03:42 +0300

    Homework #19

--- omework #19/index.js
+++ omework #19/index.js
@@ -1,1 +1,17 @@
 //Homework #19
+
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this
+    },
+    down: function () {
+        this.step--;
+        return this
+    },
+    showStep: function () { 
+        alert(this.step);
+        return this
+    }
+};
